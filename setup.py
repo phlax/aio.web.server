150cb4ef0b936b4e8d65e323b413be73ddc0e2f1
Ryan Northey <ryan@space23.net>
2015-05-22 22:24:01 +0000

    adding aio.conf in setup

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(),
     namespace_packages=['aio', "aio.web"],
     include_package_data=True,
-    package_data={'': ['templates/*.html', 'templates/fragments/*.html', '*.rst']},
+    package_data={'': ['templates/*.html', 'templates/fragments/*.html', '*.rst', "aio.conf"]},
     test_suite="aio.app.tests",
     zip_safe=False,
     tests_require=tests_require,
