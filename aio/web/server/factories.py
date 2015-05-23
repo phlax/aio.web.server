8f03f05dc7a2469b043572c91235a12c00b7070d
Ryan Northey <ryan@space23.net>
2015-05-23 01:07:00 +0000

    adding tests for web server factory methods

--- aio/web/server/factories.py
+++ aio/web/server/factories.py
@@ -7,7 +7,7 @@ from zope.dottedname.resolve import resolve
 import aiohttp_jinja2
 import jinja2
 
-from aio.core.exceptions import MissingConfiguration
+from aio.core.exceptions import MissingConfiguration, BadConfiguration
 import aio.app
 
 import logging
@@ -18,63 +18,74 @@ class FileSystemLoader(jinja2.FileSystemLoader):
     pass
 
 
-@asyncio.coroutine
-def filters_factory(webapp, conf):
-    if conf.get("filters"):
-        env = aiohttp_jinja2.get_env(webapp)
-        for filt in conf['filters'].split("\n"):
-            name, handler = [x for x in filt.split(" ") if x]
-            env.filters[name] = resolve(handler)
-
-
-@asyncio.coroutine
-def static_factory(webapp, conf):
-    webapp['static'] = []
-
-    if "static_url" in conf and "static_dir" in conf:
-        log.debug("Setting up static folders for: %s" % webapp['name'])
-        webapp.router.add_static(
-            conf['static_url'],
-            os.path.abspath(conf['static_dir']))
-
-        for module in aio.app.modules:
-            path = os.path.join(
-                module.__path__[0], "static")
-            if os.path.exists(path):
-                webapp['static'].append((module.__name__, path))
-
-
-@asyncio.coroutine
-def templates_factory(webapp, conf):
-    templates = []
-
+def get_factory_modules(conf):
+    factory_modules = ''
     try:
-        template_modules = aio.app.config['web/%s']['modules']
+        factory_modules = conf['modules']
     except KeyError:
         try:
-            template_modules = aio.app.config["aio/web"]["modules"]
+            factory_modules = aio.app.config["aio/web"]["modules"]
         except KeyError:
             try:
-                template_modules = aio.app.config["aio"]["modules"]
+                factory_modules = aio.app.config["aio"]["modules"]
             except KeyError:
-                template_modules = ''
-
-    if not template_modules:
-        return
-
-    log.debug("Setting up templates for: %s" % webapp['name'])
-
-    for module in [x.strip() for x in template_modules.split("\n")]:
-        templates.append(
-            os.path.join(
-                resolve(module).__path__[0], "templates"))
-    aiohttp_jinja2.setup(webapp, loader=FileSystemLoader(templates))
+                pass
+        except TypeError:
+            pass
+    except TypeError:
+        pass
+    return [
+        x.strip() for x in
+        factory_modules.split("\n") if x.strip()]
 
 
 @asyncio.coroutine
-def routes_factory(webapp, conf):
-    log.debug("Setting up routes for: %s" % webapp['name'])
-    route_definitions = "web/%s/" % webapp['name']
+def filters_factory(web_app, conf):
+    if conf.get("filters"):
+        env = aiohttp_jinja2.get_env(web_app)
+        for filt in conf['filters'].split("\n"):
+            name, handler = [x for x in filt.split(" ") if x]
+            try:
+                env.filters[name] = resolve(handler)
+            except ImportError:
+                raise BadConfiguration("Cannot import filter %s (%s)" % (
+                    name, handler))
+
+
+@asyncio.coroutine
+def static_factory(web_app, conf):
+    web_app['static'] = []
+
+    if "static_url" in conf and "static_dir" in conf:
+        log.debug("Setting up static folders for: %s" % web_app['name'])
+        static_dir = conf['static_url']
+        static_url = os.path.abspath(conf['static_dir'])
+        web_app.router.add_static(
+            static_dir, static_url)
+
+
+@asyncio.coroutine
+def templates_factory(web_app, conf):
+    templates = []
+    template_modules = get_factory_modules(conf)
+    if template_modules:
+        log.debug("Setting up templates for: %s" % web_app['name'])
+        for module in template_modules:
+            templates.append(
+                os.path.join(
+                    resolve(module).__path__[0], "templates"))
+        aiohttp_jinja2.setup(web_app, loader=FileSystemLoader(templates))
+    else:
+        aiohttp_jinja2.setup(web_app)
+    yield from filters_factory(web_app, conf)
+
+
+@asyncio.coroutine
+def routes_factory(web_app, conf):
+    log.debug("Setting up routes for: %s" % web_app['name'])
+    if not aio.app.config:
+        return
+    route_definitions = "web/%s/" % web_app['name']
     route_sections = [
         (x, aio.app.config[x]) for x in aio.app.config.sections()
         if x.startswith(route_definitions)]
@@ -98,7 +109,7 @@ def routes_factory(webapp, conf):
 
         log.debug('adding route (%s): %s %s %s' % (
             name, method, match, handler))
-        webapp.router.add_route(method, match, handler, name=name)
+        web_app.router.add_route(method, match, handler, name=name)
 
 
 @asyncio.coroutine
